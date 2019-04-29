ceea9b3175992430108b79a666039cafb92ba1b4
Marcin <31897007+marcin-wski@users.noreply.github.com>
2019-04-29 14:25:20 -0700

    Adding handlers

--- Ansible/walk-through.sh
+++ Ansible/walk-through.sh
@@ -10,7 +10,8 @@ sudo yum install ansible
   hosts: web                      #### Host selector
     become: yes                   #### Privilege escalation to root
     vars:                         #### Variables used by the playbook
-      http_port: 80               
+      http_port: 80
+      vhost: Ansible-Test-VM
 tasks:                            #### Tasks performed by the playbook
 - name: httpd package is present
   yum:
@@ -21,6 +22,39 @@ tasks:                            #### Tasks performed by the playbook
     src: files/index.html
     dest: /var/www/html/
     
+tasks:                            #### Playing around with using vars
+  - name: create a virtual host file for {{ vhost }}
+    template:
+      src: somefile.j2
+      dest: /etc/httpd/conf.d/{{ vhost }}
+
+tasks:                            #### How to use "command" option
+  - name: enable selinux
+    command: /sbin/setenforce 1
+    
+tasks:                            #### How to use "shell" option
+  - name: run this command and ignore the result
+    shell: /usr/bin/somecommand
+    ignore_errors: True
+
+# Handlers
+handlers:
+    - name: restart memcached
+      service:
+        name: memcached
+        state: restarted
+      listen: "restart web services"
+    - name: restart apache
+      service:
+        name: apache
+        state:restarted
+      listen: "restart web services"
+
+tasks:
+    - name: restart everything
+      command: echo "this task will restart the web services"
+      notify: "restart web services"
+      
 #Loops
 - yum:
 name: "{{ item }}"
@@ -40,6 +74,12 @@ allow_duplicates: true
 
 #Do you not care about errors?
 ignore_errors: True
+
+#Check hosts affected
+ansible-playbook playbook.yml --list-hosts
+
+#Run playbook
+ansible-playbook playbook.yml
 
 #https://docs.ansible.com/ansible/latest/user_guide/playbooks_intro.html
 #need to actually read up on using the roles:
