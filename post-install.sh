fb23cceb27987b9013570ab31702c1fba01f72d2
Marcin <31897007+marcin-wski@users.noreply.github.com>
2019-07-28 16:19:08 -0700

    Adding things required by multibootusb

--- post-install.sh
+++ post-install.sh
@@ -7,9 +7,9 @@ echo "Doing this as root"
 dnf install  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 dnf groupupdate core -y
-dnf remove @gnome-desktop -y
 dnf install @kde-desktop -y
-dnf install wget tmux htop net-tools sysstat openssh-server nmap vlc java-openjdk icedtea-web gimp youtube-dl unzip bridge-utils libvirt virt-install qemu-kvm virt-top libguestfs-tools virt-manager virsh transmission discord libreoffice libreoffice-langpack-en libreoffice-langpack-pl aisleriot steam -y
+dnf remove @gnome-desktop -y
+dnf install wget tmux htop net-tools sysstat openssh-server nmap vlc java-openjdk icedtea-web gimp youtube-dl unzip p7zip p7zip-plugins python3-PyQt5 python3 bridge-utils libvirt virt-install qemu-kvm virt-top libguestfs-tools virt-manager virsh transmission discord libreoffice libreoffice-langpack-en libreoffice-langpack-pl aisleriot steam -y
 wget https://github.com/mbusb/multibootusb/releases/download/v9.2.0/multibootusb-9.2.0-1.noarch.rpm
 rpm -i multibootusb-9.2.0-1.noarch.rpm
 systemctl start firewalld
