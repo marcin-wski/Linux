3c0d2521695d4463a977029bc103f891c205a680
Marcin <31897007+marcin-wski@users.noreply.github.com>
2019-08-04 14:40:56 -0700

    Fixing issue with desktop manager

--- post-install.sh
+++ post-install.sh
@@ -1,6 +1,7 @@
-#version 0.4
+#version 0.6
 #testing on Fedora 30 now
-#testing proved that some stuff is not configured properly. More updates to follow
+#It appears that the power management does not currently start properly
+#More updates to follow
 
 sudo -s <<EOF
 
@@ -9,7 +10,6 @@ dnf install  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-
 dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 dnf groupupdate core -y
 dnf install @kde-desktop -y
-dnf install sddm -y
 dnf remove @gnome-desktop -y
 dnf groupremove "GNOME Desktop Environment" -y
 dnf remove gdm -y
@@ -22,6 +22,9 @@ dnf install libreoffice libreoffice-langpack-en libreoffice-langpack-pl -y
 dnf install aisleriot steam -y
 dnf install nvidia*3:430* -y
 dnf install nvidia-texture-tools -y
+sudo dnf remove sddm -y
+sudo dnf install sddm -y
+sudo dnf install kdm -y
 wget https://github.com/mbusb/multibootusb/releases/download/v9.2.0/multibootusb-9.2.0-1.noarch.rpm
 rpm -i multibootusb-9.2.0-1.noarch.rpm
 systemctl start firewalld
